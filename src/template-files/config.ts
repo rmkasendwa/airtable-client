e96b8dd50ca270d05464d6bd307c9d068ff99cbd
Ronald M. Kasendwa <ronald.kasendwa@theoremone.co>
2023-01-09 23:28:23 +0300

    Publishes to @theoremone-talent github registry

--- src/template-files/config.ts
+++ src/template-files/config.ts
@@ -3,4 +3,5 @@ dotenv.config();
 
 export const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 export const AIRTABLE_BASE_ID =
+  process.env.AIRTABLE_BASE_ID ||
   /* AIRTABLE_BASE_ID */ process.env.AIRTABLE_BASE_ID; /* AIRTABLE_BASE_ID */
