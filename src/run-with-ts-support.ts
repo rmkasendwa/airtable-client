d9a3d16f8f529be97677ec0cba05ac5caf4eb178
Ronald M. Kasendwa <ronald.kasendwa@theoremone.co>
2023-07-01 02:21:03 +0300

    Implements typescript support in airtable api configuration file

---
+++ src/run-with-ts-support.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+import '@infinite-debugger/rmk-js-extensions/RegExp';
+import '@infinite-debugger/rmk-js-extensions/String';
+
+import { join, normalize } from 'path';
+
+import { existsSync } from 'fs-extra';
+
+import { generateAirtableAPI } from './api';
+import { Config } from './models';
+
+const currentWorkingDirectory = process.cwd();
+
+const pkg = (() => {
+  if (existsSync(`${__dirname}/package.json`)) {
+    return require(`${__dirname}/package.json`);
+  }
+  const pkgPath = join(__dirname, '..', 'package.json');
+  if (existsSync(pkgPath)) {
+    return require(pkgPath);
+  }
+})();
+
+const args = process.argv;
+
+if (args.includes('-G') || args.includes('--generate')) {
+  const generateAllTables = args.includes('--all');
+  const userConfigFilePath = `${currentWorkingDirectory}/airtable-api.config`;
+
+  if (
+    ['.json', '.js', '.ts'].some((fileExtension) =>
+      existsSync(userConfigFilePath + fileExtension)
+    )
+  ) {
+    const userConfig: Config<string> = (() => {
+      const config = require(userConfigFilePath);
+      if (config.default) {
+        return config.default;
+      }
+      return config;
+    })();
+
+    const outputRootPath = (() => {
+      if (args.includes('-o')) {
+        const argsPath = args[args.indexOf('-o') + 1];
+        if (argsPath) {
+          return normalize(join(currentWorkingDirectory, argsPath));
+        }
+      }
+      return currentWorkingDirectory;
+    })();
+
+    generateAirtableAPI({
+      userConfig,
+      outputRootPath,
+      generateAllTables,
+    });
+  } else {
+    console.log('API configuration file not found. Exiting...');
+  }
+} else {
+  console.log(`${pkg.name} v${pkg.version}`);
+}
--- src/run.ts
+++ src/run.ts
@@ -1,60 +1,9 @@
-#!/usr/bin/env node
+import { exec } from 'node:child_process';
 
-import '@infinite-debugger/rmk-js-extensions/RegExp';
-import '@infinite-debugger/rmk-js-extensions/String';
+const currentProcessArgs = process.argv.slice(2);
 
-import { join, normalize } from 'path';
-
-import { existsSync } from 'fs-extra';
-
-import { generateAirtableAPI } from './api';
-import { Config } from './models';
-
-const currentWorkingDirectory = process.cwd();
-
-const pkg = (() => {
-  if (existsSync(`${__dirname}/package.json`)) {
-    return require(`${__dirname}/package.json`);
-  }
-  const pkgPath = join(__dirname, '..', 'package.json');
-  if (existsSync(pkgPath)) {
-    return require(pkgPath);
-  }
-})();
-
-const args = process.argv;
-
-if (args.includes('-G') || args.includes('--generate')) {
-  const generateAllTables = args.includes('--all');
-  const userConfigFilePath = `${currentWorkingDirectory}/airtable-api.config.js`;
-
-  if (existsSync(userConfigFilePath)) {
-    const userConfig: Config<string> = (() => {
-      const config = require(userConfigFilePath);
-      if (config.default) {
-        return config.default;
-      }
-      return config;
-    })();
-
-    const outputRootPath = (() => {
-      if (args.includes('-o')) {
-        const argsPath = args[args.indexOf('-o') + 1];
-        if (argsPath) {
-          return normalize(join(currentWorkingDirectory, argsPath));
-        }
-      }
-      return currentWorkingDirectory;
-    })();
-
-    generateAirtableAPI({
-      userConfig,
-      outputRootPath,
-      generateAllTables,
-    });
-  } else {
-    console.log('API configuration file not found. Exiting...');
-  }
-} else {
-  console.log(`${pkg.name} v${pkg.version}`);
-}
+const { stdout, stderr } = exec(
+  `tsnd ${__dirname}/run-with-ts-support ${currentProcessArgs.join(' ')}`
+);
+stdout?.on('data', (data) => console.log(data));
+stderr?.on('data', (data) => console.error(data));
