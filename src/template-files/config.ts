f417c3e6c992df7d68e92a4dd4e1509075c919fb
Ronald M. Kasendwa <ronald.kasendwa@theoremone.co>
2023-09-19 00:08:45 +0300

    Adds ORIGINAL_AIRTABLE_BASE_ID variable in generated api, improves APIGenerator implementation

--- src/template-files/config.ts
+++ src/template-files/config.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-export const AIRTABLE_BASE_ID =
-  process.env.AIRTABLE_BASE_ID ||
+export const ORIGINAL_AIRTABLE_BASE_ID =
   /* AIRTABLE_BASE_ID */ process.env.AIRTABLE_BASE_ID; /* AIRTABLE_BASE_ID */
+export const AIRTABLE_BASE_ID =
+  process.env.AIRTABLE_BASE_ID || ORIGINAL_AIRTABLE_BASE_ID;
