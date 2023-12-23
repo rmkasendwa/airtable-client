179e3a59310c4989d94fee100f472fd2cab7c98e
Ronald M. Kasendwa <ronald.kasendwa@theoremone.co>
2023-12-23 03:08:05 +0300

    Generates `Current Role` field dependency report

--- src/__playground__/find-all-field-references.playground.ts
+++ src/__playground__/find-all-field-references.playground.ts
@@ -1,5 +1,10 @@
+import { ensureDirSync, writeFileSync } from 'fs-extra';
+
 import { findAllTableFieldReferences } from '../api';
 import userConfig from './airtable-api.config';
+
+const DUMP_DIR = `${__dirname}/dump`;
+ensureDirSync(DUMP_DIR);
 
 (async () => {
   const fieldReferences = await findAllTableFieldReferences({
@@ -8,5 +13,43 @@ import userConfig from './airtable-api.config';
     fieldIdOrName: 'Current Role',
     userConfig,
   });
-  console.log({ fieldReferences });
+  fieldReferences.sort(
+    ({ tableName: aTableName }, { tableName: bTableName }) => {
+      return aTableName.localeCompare(bTableName);
+    }
+  );
+
+  //#region Group field references by table and output list of fields as text
+  const fieldReferencesByTable = fieldReferences.reduce<
+    Record<string, string[]>
+  >((acc, { tableName, fieldName }) => {
+    if (!acc[tableName]) {
+      acc[tableName] = [];
+    }
+    acc[tableName].push(fieldName);
+    return acc;
+  }, {});
+  const fieldReferencesByTableText = Object.entries(fieldReferencesByTable)
+    .reduce<string[]>((acc, [tableName, fieldNames]) => {
+      acc.push(
+        [
+          `${tableName}:`,
+          fieldNames
+            .map((fieldName) => {
+              return ` -> ${fieldName}`;
+            })
+            .join('\n'),
+        ].join('\n')
+      );
+      return acc;
+    }, [])
+    .join('\n\n');
+  //#endregion
+
+  //#region Dump fields likely to be affected if the `Current Role` field is changed
+  writeFileSync(
+    `${DUMP_DIR}/fields-likely-to-be-affected-if-current-role-field-is-changed.dump`,
+    fieldReferencesByTableText
+  );
+  //#endregion
 })();
